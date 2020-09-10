8465579dbf3fc5c2dcae621c1d63d32c21cbb4e9
stephen shen <723809534@qq.com>
2020-09-10 15:16:25 +0800

    完成全局数据验证

--- src/main/java/com/qfedu/common/CommonException.java
+++ src/main/java/com/qfedu/common/CommonException.java
@@ -1,5 +1,13 @@
 package com.qfedu.common;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.Iterator;
+
 /**
  * @description
  * @className: CommonException
@@ -7,5 +15,28 @@ package com.qfedu.common;
  * @author: Stephen Shen
  * @date: 2020/9/10 下午2:15
  */
+//控制器增强
+//也需要扫描
+@ControllerAdvice
+@ResponseBody
 public class CommonException {
+
+    //修饰方法，表示一个异常处理的方法 返回一个json数据 {code:0 info：“账号不可为空”}
+    @ExceptionHandler(ConstraintViolationException.class)
+    public JsonResult ConstraintViolationException(ConstraintViolationException ex){
+        Iterator<ConstraintViolation<?>> iterator = ex.getConstraintViolations().iterator();
+        String message="";
+        if (iterator .hasNext()) {
+            message = iterator.next().getMessage();
+        }
+
+        return new JsonResult(0, message);
+    }
+
+    //处理哪些没有意料到的异常
+    @ExceptionHandler
+    public JsonResult commonException(Exception ex){
+        return new JsonResult(0,ex.getMessage());
+    }
+
 }
--- src/main/java/com/qfedu/controller/LoginController.java
+++ src/main/java/com/qfedu/controller/LoginController.java
@@ -4,12 +4,20 @@ import com.qfedu.common.JsonResult;
 import com.qfedu.entity.User;
 import com.qfedu.service.UserService;
 import com.qfedu.utils.StrUtils;
+import org.aspectj.bridge.IMessage;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.constraints.Size;
+import java.util.Iterator;
 
 /**
  * @description
@@ -19,6 +27,8 @@ import javax.servlet.http.HttpSession;
  * @date: 2020/9/9 下午2:24
  */
 @Controller
+//如果在方法参数上使用验证注解，赢要在这个类上写这个注解@Validated
+@Validated
 public class LoginController {
 
     @Autowired
@@ -26,10 +36,12 @@ public class LoginController {
 
     @RequestMapping("/login.do")
     @ResponseBody
-    public JsonResult login(String bankCode, String password, HttpSession session) {
+    public JsonResult login(@NotEmpty(message = "{name.empty}") String bankCode, @Size(min = 1,max = 10) String password, HttpSession session) {
         User user = userService.login(bankCode, password);
         session.setAttribute(StrUtils.LOGIN_USER, user);
         JsonResult jsonResult = new JsonResult(1, null);
         return jsonResult;
     }
+
+
 }
