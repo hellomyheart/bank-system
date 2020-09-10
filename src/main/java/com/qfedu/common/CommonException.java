6c98687a9718f26468a103e88c7ebbf5b716eec7
stephen shen <723809534@qq.com>
2020-09-10 15:15:55 +0800

    完成全局数据验证

---
+++ src/main/java/com/qfedu/common/CommonException.java
@@ -0,0 +1,11 @@
+package com.qfedu.common;
+
+/**
+ * @description
+ * @className: CommonException
+ * @package: com.qfedu.common
+ * @author: Stephen Shen
+ * @date: 2020/9/10 下午2:15
+ */
+public class CommonException {
+}
